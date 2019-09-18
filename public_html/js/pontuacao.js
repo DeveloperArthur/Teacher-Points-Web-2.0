31a6900ddf0a644455c2cc31dd195b2083aad32f
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-09-18 11:33:53 -0300

    Update pontuacao.js

--- public_html/js/pontuacao.js
+++ public_html/js/pontuacao.js
@@ -8,69 +8,41 @@ function calcularPontuacao(){
 	//Tratativa de erro caso usuario deixe algum campo vazio
 	if(mes_cargo == ''){
 		document.getElementById('erroCargo').innerHTML = 'Preencha todos os campos!';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 	if(mes_lotacao == ''){
 		document.getElementById('erroLotacao').innerHTML = 'Preencha todos os campos!';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 	if(mes_adjunto == ''){
 		document.getElementById('erroAdjunto').innerHTML = 'Preencha todos os campos!';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	} 
 	if(mes_titular == ''){
 		document.getElementById('erroTitular').innerHTML = 'Preencha todos os campos!';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}   
 	if(magisterio == ''){
 		document.getElementById('erroMagisterio').innerHTML = 'Preencha todos os campos!';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 
 	//Tratativa de erro se usuario digitar numero negativo
 	if(mes_cargo < 0){
 		document.getElementById('erroCargo').innerHTML = 'Digite somente números positivos';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 	if(mes_lotacao < 0){
 		document.getElementById('erroLotacao').innerHTML = 'Digite somente números positivos';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 	if(mes_adjunto < 0){
 		document.getElementById('erroAdjunto').innerHTML = 'Digite somente números positivos';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	} 
 	if(mes_titular < 0){
 		document.getElementById('erroTitular').innerHTML = 'Digite somente números positivos';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}   
 	if(magisterio < 0){
 		document.getElementById('erroMagisterio').innerHTML = 'Digite somente números positivos';
-		document.getElementById('total1').innerHTML = '';
-	    document.getElementById('total2').innerHTML = '';
 	}
 	
-	if(
-		mes_cargo != '' && 
-		mes_lotacao != '' && 
-		mes_adjunto != '' && 
-		mes_titular != '' && 
-		magisterio != '' && 
-		mes_cargo > 0 && 
-		mes_lotacao > 0 && 
-		mes_adjunto > 0 && 
-		mes_titular > 0 && 
-		magisterio > 0
-	){
+	document.getElementById('total1').innerHTML = '';
+    document.getElementById('total2').innerHTML = '';
+
+	if(mes_cargo != '' && mes_lotacao != '' && mes_adjunto != '' && mes_titular != '' && magisterio != '' && mes_cargo > 0 && mes_lotacao > 0 && mes_adjunto > 0 && mes_titular > 0 && magisterio > 0){
 		document.getElementById('erroCargo').innerHTML = '';
 		document.getElementById('erroLotacao').innerHTML = '';
 		document.getElementById('erroAdjunto').innerHTML = '';
@@ -88,7 +60,5 @@ function calcularPontuacao(){
 
 		document.getElementById('total1').innerHTML = total1;
 	    document.getElementById('total2').innerHTML = total2;
-	}else{
-		alert('Você não preencheu o formulário corretamente');
 	}		
 }
